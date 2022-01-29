a908e6828e8926a3cc30b1f654a65a4bc256fc0e
peakTime <2441636874@qq.com>
2022-01-29 23:01:31 +0800

    修改了axios

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,13 @@
 
 <script>
 export default {
-
+  created () {
+    this.$http.get('/search?keywords=海阔天空', {
+      withCredentials: true
+    }).then(val => {
+      console.log(val)
+    })
+  }
 }
 </script>
 
---
+++ src/axios/index.js
@@ -0,0 +1,3 @@
+import axios from 'axios'
+axios.default.baseURL = 'https://netease-cloud-music-api-omega-topaz.vercel.app/'
+export default axios
