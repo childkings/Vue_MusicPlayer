9a2d291b9dcc2e3ba1aa3e021e54e23d73ba33fc
peakTime <2441636874@qq.com>
2022-02-02 01:20:57 +0800

    完成了Header的基本样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,21 @@
 <template>
-  <div></div>
+  <div>
+    <component :is="header"></component>
+  </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      header: 'my-header'
+    }
+  }
 }
 </script>
 
-<style>
-
+<style lang="less" scoped>
+body{
+  background-color: rgb(245, 245, 245);
+}
 </style>
