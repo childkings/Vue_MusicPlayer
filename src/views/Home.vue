96aa1ffc6c00513ede7e0b09f0fed653d10866d0
peakTime <2441636874@qq.com>
2022-02-04 00:52:24 +0800

    令人头疼的audio基本样式完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,15 +2,20 @@
   <div>
     <component :is="header"></component>
     <router-view></router-view>
+    <MusicPlayer></MusicPlayer>
   </div>
 </template>
 
 <script>
+import MusicPlayer from '@/components/MusicPlayer.vue'
 export default {
   data () {
     return {
       header: 'my-header'
     }
+  },
+  components: {
+    MusicPlayer
   }
 }
 </script>
